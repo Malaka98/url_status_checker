cbc8b4cf2fd5fc75e3488695d108b393c0134d15
Malaka98 <malaka.aruna14413@gmail.com>
2021-05-27 17:44:16 +0530

    Fix

--- controllers/check.js
+++ controllers/check.js
@@ -55,62 +55,52 @@ var respons = [
   },
 ];
 
-const testUrl = [
-  "fossnsbm.org",
-  "forum.fossnsbm.org",
-]
+const testUrl = ["fossnsbm.org", "forum.fossnsbm.org"];
 
 exports.check = async (reqq, resp, next) => {
-
   const statusCheck = (url) => {
     return new Promise((resolve, reject) => {
-
       const https = require("https");
-    
+
       const options = {
         hostname: url,
         path: "/",
         method: "GET",
       };
-    
+
       const req = https.request(options, (res) => {
         console.log(`statusCode: ${res.statusCode}`);
         if (res.statusCode == 200) {
-          setTimeout(() => {
-            
-              resolve("Active");
-            
-          }, 16000);
+          resolve("Active");
         } else {
           resolve("Inactive");
         }
       });
-    
+
       req.on("error", (error) => {
         console.error(error);
         // respons[id].status = "Loading";
       });
-    
+
       req.end();
     });
-  }
-  
+  };
 
-  await Promise.all(testUrl.map((value, key) => {
-    return statusCheck(value);
-  })).then(result => {
+  await Promise.all(
+    testUrl.map((value, key) => {
+      return statusCheck(value);
+    })
+  ).then((result) => {
     respons[0].status = result[0];
     respons[1].status = result[1];
     // console.log(result);
-  })
-  
+  });
 
-    if(reqq.timedout) {
+  if (reqq.timedout) {
     next();
   } else {
-    resp.json({statusData:respons});
+    resp.json({ statusData: respons });
   }
-  
+
   // resp.send("Hello World");
 };
-
